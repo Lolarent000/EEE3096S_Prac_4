4c2b7f9b5ac73b8d2bd965d28ecb87215b72f21c
Lolarent000 <2me.alan@gmail.com>
2018-09-13 15:37:15 +0000

    Added first 2 button functionality and Variables

--- prac_4_PHLALA005_HDGTIM003.py
+++ prac_4_PHLALA005_HDGTIM003.py
@@ -4,6 +4,38 @@ import spidev
 import time
 import os
 import sys
+import RPi.GPIO as GPIO
+
+# Define sensor channels
+channel = 0
+
+# Define delay between readings in sec
+delay = 0.5
+delay_table = [0.5, 1, 2]
+
+# Define array which stores first 5 variables
+storage = [[],[],[],[],[]]
+
+# Define variable to track stop: 0 stop is off, 1 stop is on
+stop = 0
+
+# Define Timer Variable in sec
+time = 0
+
+# Setup GPIO for buttons
+GPIO.setmode(GPIO.BCM)
+
+# Setup pin numbers
+reset_pin = 17
+freq_pin = 18
+stop_pin=22
+display_pin=23
+
+# Setup pins
+GPIO.setup(reset_pin, GPIO.IN, pull_up_pull_down=GPIO.PUD_UP)
+GPIO.setup(freq_pin, GPIO.IN, pull_up_pull_down=GPIO.PUD_UP)
+GPIO.setup(stop_pin, GPIO.IN, pull_up_pull_down=GPIO.PUD_UP)
+GPIO.setup(display_pin, GPIO.IN, pull_up_pull_down=GPIO.PUD_UP)
 
 # Open SPI bus
 spi = spidev.SpiDev() # create spi object
@@ -23,13 +55,27 @@ def ConvertVolts(data,places):
 	volts = round(volts,places)
 	return volts
 
-# Define sensor channels
-channel = 0
+# Functions for buttons
+def reset():
+	time = 0
+	storage = [[],[],[],[],[]]
+	print("Time      Timer      Pot      Temp      Light")
 
-# Define delay between readings
-delay = .5
+def freq():
+	id = delay_table.index(delay)
+	if (id+1 >= len(delay_table):
+		delay = delay_table[0]
+	else:
+		delay = delay_table[id+1]
+
+# Setup GPIO interrupts
+GPIO.add_event_detect(reset_pin, GPIO.FALLING, callback=reset, bouncetime=200)
+GPIO.add_event_detect(freq_pin, GPIO.FALLING, callback=freq, bouncetime=200)
+GPIO.add_event_detect(stop_pin, GPIO.FALLING, callback=stop, bouncetime=200)
+GPIO.add_event_detect(display_pin, GPIO.FALLING, callback=display, bouncetime=200)
 
 try:
+	print("Time      Timer      Pot      Temp      Light")
 	while True:
 		# Read the data
 		sensr_data = GetData (channel)
@@ -37,8 +83,10 @@ try:
 
 		# Wait before repeating loop
 		time.sleep(delay)
+
 except Exception as e:
 	print(e)
 
 finally:
 	spi.close()
+	GPIO.cleanup()
